59daab6e20a8da319034106d4205ce89dd7f2c19
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2021-02-20 21:40:01 +0100

    Create and update admin.py in listings/ realtors/

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Listing
 
+# Ta klasa pozwoli nam zmienic wyglad w panelu admin, tu: http://127.0.0.1:8000/admin/listings/listing/
+# To dzieki niej mamy tą tabelke
+class ListingAdmin(admin.ModelAdmin):
+    # Podajemy wartosci do tabelki; ta zmienna musi sie tak nazywac
+    list_display = ('id', 'title', 'is_published', 'price', 'realtor')
+
+    # Tu podajemy w co chcemy miec mozliwosc kliknac po wiecej szczegolow 
+    list_display_links = ('id', 'title', 'is_published', 'price', 'realtor')
+
+    # Tu podajemy przez co chcemy miec mozliwosc filtrowania tej tabeli, to to po prawej
+    # Gdy mamy jeden parametr na koncu MUSI byc przecinek; nwm czemu, ale musi
+    list_filter = ('realtor',)
+
+    # To sprawia ze mamy search bar, w nawiasach sa wartosci po ktorych mozemy szukac
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+
+    list_per_page = 25
 # Dodajemy teraz tabele listings do panelu admina
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Realtor
 
+# Wyjasnienia wszystkiego w listings/admin.py
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'is_mvp', 'hire_date')
+    list_display_links = ('id', 'name', 'email', 'is_mvp', 'hire_date')
+    list_filter = ('is_mvp',)
+    search_fields = ('name', 'email')
+    list_per_page = 25
+
+
 
 # Dodajemy teraz tabele Realtor do panelu admina
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
