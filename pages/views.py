6a197c27dc80ca12dae2d1f29a559dca40905e4f
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2021-02-25 19:54:23 +0100

    Create backend choices in search form

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from realtors.models import Realtor
 from listings.models import Listing
+from listings.choices import price_choices, bedroom_choices, state_choices
 # Create your views here.
 
 
@@ -11,7 +12,10 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     data = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
     }
     
     return render(request, 'pages/index.html', data)
