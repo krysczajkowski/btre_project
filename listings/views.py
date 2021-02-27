e6ad95831a048bfae59762ddb2a73925d9d85333
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2021-02-27 09:04:34 +0100

    Preserving form input

--- listings/views.py
+++ listings/views.py
@@ -37,10 +37,61 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', data)
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Sprawdzamy czy sa jakies keywordy w zapytaniu get (to ten search)
+    if 'keywords' in request.GET:
+        # Btw. te nazwy 'city', 'keywords' bierzemy z atrybutu name w formularzu html z pliku search
+        keywords = request.GET['keywords']
+
+        if keywords:
+            # Sprawdzamy czy w polu description w bazie danych sa keywordsy ktore wpisal uzytkownik
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # Miasto
+    if 'city' in request.GET:
+        city = request.GET['city']
+
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # Stan
+    if 'state' in request.GET:
+        state = request.GET['state']
+
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+
+    # Sypialnie
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+
+        if bedrooms:
+            # Funkcja lte wybierze nam wszystkie lazienki DO tej cyfry
+            # Czyli jesli wybrales 4, to ona pokaze mieszkania z 1,2,3 i 4
+            # Jest bedroom__lte bo w bazie danych to pole nazywa sie bedroom, a w url bedrooms
+            # Tu odowolujemy sie do bazy danych, wiec 
+            queryset_list = queryset_list.filter(bedroom__lte=bedrooms)
+
+
+    # Cena
+    if 'price' in request.GET:
+        price = request.GET['price']
+
+        if price:
+            # Funkcja lte wybierze nam wszystkie lazienki DO tej cyfry
+            # Czyli jesli wybrales 4, to ona pokaze mieszkania z 1,2,3 i 4
+            # Jest bedroom__lte bo w bazie danych to pole nazywa sie bedroom, a w url bedrooms
+            # Tu odowolujemy sie do bazy danych, wiec 
+            queryset_list = queryset_list.filter(price__lte=price)
+
     data = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
     }
 
     return render(request, 'listings/search.html', data)
